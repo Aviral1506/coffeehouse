a39eb156648f752d33208e699de127a5e35984dd
Aviral Agarwal <43955898+Aviral1506@users.noreply.github.com>
2026-06-13 02:36:21 +0530

    fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,6 +60,7 @@ dependencies {
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.ui:ui-tooling-preview")
     debugImplementation("androidx.compose.ui:ui-tooling")
+    implementation("com.google.android.material:material:1.12.0")
 
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.9.0")
     implementation("androidx.datastore:datastore-preferences:1.1.4")
